f423f2b57c0d4eb52f4b579ebe7dd5e41b0f5440
Aldo Saenz <aldosaenz518@gmail.com>
2025-09-13 17:10:04 -0500

    Added Hero Card and Course Cards

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Link from "next/link";
-import { User, Settings } from "lucide-react";
+import { CircleUserRound, Settings } from "lucide-react";
+
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between items-center py-8 px-8 bg-gray-100 border-b border-gray-300">
+            <div className="flex gap-4">
+                <Link href="/settings" className="group relative">
+                    <Settings size={48} className="cursor-pointer text-gray-900"/>
+                </Link>
+            </div>
+
+            <div>
+                <Link href="https://youtu.be/dQw4w9WgXcQ?si=40stDI22AECNIj0-" className="font-bold text-5xl">
+                    <span  className="cursor-pointer text-gray-900">DevGPA</span>
+                </Link>
+            </div>
+
+            <div className="flex gap-4">
+                <Link href="/profile" className="group relative">
+                    <CircleUserRound size={48} className="cursor-pointer text-gray-900"/>
+                </Link>
+            </div>
+        </nav>
     );
 }
 
